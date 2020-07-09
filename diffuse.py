44465bcd269d0578a649bc6f6faa202d42eddaa3
nidosail <63743206+nidosail@users.noreply.github.com>
2020-07-09 16:10:22 -0700

    Update diffuse.py

--- diffuse.py
+++ diffuse.py
@@ -1,6 +1,7 @@
-import numpy as np
+from itertools import product
 
-def around6(loc):
-    listoflocs = np.array([loc])
-    for num in loc:
-        np.append(listoflocs,[[num]])
+def neighbors(index): # this is a function for finding the neighbor indices of a given index in a matrix for cytokine diffusion.
+    N = len(index) # This has ben copy pasted lmao
+    for relative_index in product((-1, 0, 1), repeat=N):
+        if not all(i == 0 for i in relative_index):
+            yield tuple(i + i_rel for i, i_rel in zip(index, relative_index))
